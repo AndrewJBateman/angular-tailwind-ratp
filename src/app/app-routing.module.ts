e5ce25a8162cc7d0a035ea9c376653402ad245c7
Andrew Bateman <gomezbateman@yahoo.com>
2021-03-20 18:17:46 +0100

    router path corrected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,6 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
     component: HomeComponent,
   },
   {
