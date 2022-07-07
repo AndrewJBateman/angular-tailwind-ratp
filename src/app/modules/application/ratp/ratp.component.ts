4c53081806bb6850affdab5eea4894fce2ab0619
AndrewJBateman <gomezbateman@yahoo.com>
2022-07-07 19:04:37 +0200

    refactor: async pipe rer observables in template

--- src/app/modules/application/ratp/ratp.component.ts
+++ src/app/modules/application/ratp/ratp.component.ts
@@ -13,10 +13,8 @@ import { TrafficService } from './services/traffic.service';
 export class RatpComponent implements OnInit {
   public ratpTraffic$: Observable<any>;
   public traffic: RatpTraffic[];
-  public tramwayTraffic: RatpTraffic[];
-  public metroTraffic: RatpTraffic[];
-  public rerTraffic: RatpTraffic[];
   public id: string | null;
+  public trafficArray: RatpTraffic[] = [];
 
   public loading = true;
   defaultRatpMessage = "Trafic normal sur l'ensemble de la ligne.";
@@ -29,7 +27,6 @@ export class RatpComponent implements OnInit {
   ngOnInit(): void {
     this._Activatedroute.paramMap.subscribe((params) => {
       this.id = params.get('id');
-      console.log('id:', this.id, 'type', typeof this.id);
     });
     this.getRatpTraffic();
     this.loading = false;
@@ -37,14 +34,7 @@ export class RatpComponent implements OnInit {
 
   private getRatpTraffic = () => {
     try {
-      const trafficId = this.id;
-      console.log('traffic: ', trafficId);
-      this.trafficService.getRatpTrafficData().subscribe((dataTraffic) => {
-        this.traffic = dataTraffic.result.trafficId;
-        this.metroTraffic = dataTraffic.result.metros;
-        this.rerTraffic = dataTraffic.result.rers;
-        this.tramwayTraffic = dataTraffic.result.tramways;
-      });
+      this.ratpTraffic$ = this.trafficService.getRatpTrafficData();
     } catch (error) {
       throw error;
     }
