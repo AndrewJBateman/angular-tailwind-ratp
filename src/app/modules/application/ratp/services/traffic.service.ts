9d86333b0b390ea91b4a150aa8f96c454611143f
AndrewJBateman <gomezbateman@yahoo.com>
2024-01-16 10:00:50 +0100

    refactor: rem module, add dropdown button role & tabindex

--- src/app/modules/application/ratp/services/traffic.service.ts
+++ src/app/modules/application/ratp/services/traffic.service.ts
@@ -1,25 +1,39 @@
-import { Injectable } from '@angular/core';
+/**
+ * TrafficService class, responsible for fetching RATP traffic data from an API.
+ * It uses the HttpClient module from Angular to make HTTP requests.
+ * The getRatpTrafficData method sends a GET request to the specified API URL and
+ * returns an Observable of type RatpTrafficApiResponse.
+ * It applies the first operator to take only the first emitted value, catchError
+ * operator to handle any errors that occur during the request,
+ * and map operator to transform the response into the desired type.
+ * If the response is invalid or empty, an error is thrown.
+ */
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError, take, tap } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, first, map } from 'rxjs/operators';
 import { RatpTrafficApiResponse } from '../models/ratp-traffic';
+import { environment } from '../../../../../environments/environment';
 
-const trafficSearchUrl = 'https://api-ratp.pierre-grimaud.fr/v4/traffic';
+const trafficSearchUrl = environment.ratp.apiUrl;
 
 @Injectable({
 	providedIn: 'root',
 })
 export class TrafficService {
-	constructor(private http: HttpClient) {}
+	private http = inject(HttpClient);
 
 	getRatpTrafficData(): Observable<RatpTrafficApiResponse> {
 		return this.http.get<RatpTrafficApiResponse>(trafficSearchUrl).pipe(
-			tap((response: RatpTrafficApiResponse) =>
-				console.log('traffic response: ', response)
-			),
-			take(1),
+			first(),
 			catchError(err => {
-				throw 'error in getting API data. Details: ' + err;
+				return throwError(`Error in getting API data: ${err}`);
+			}),
+			map((response: RatpTrafficApiResponse) => {
+				if (!response) {
+					throw new Error('Invalid API response');
+				}
+				return response;
 			})
 		);
 	}
--- src/app/modules/common/nav/dropdowns/ratp-dropdown/ratp-dropdown.component.ts
+++ src/app/modules/common/nav/dropdowns/ratp-dropdown/ratp-dropdown.component.ts
@@ -1,7 +1,19 @@
+/**
+ * Component for displaying a dropdown menu for RATP (Régie Autonome des Transports Parisiens) options.
+ * This component is used in an Angular application.
+ *
+ * @selector app-ratp-dropdown
+ * @templateUrl ./ratp-dropdown.component.html
+ * @standalone true
+ * @imports RouterLinkActive, RouterLink
+ */
 import { Component } from '@angular/core';
+import { RouterLinkActive, RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-ratp-dropdown',
 	templateUrl: './ratp-dropdown.component.html',
+	standalone: true,
+	imports: [RouterLinkActive, RouterLink],
 })
 export class RatpDropdownComponent {}
