2cbb9e6a41011349c13ff9648a7239a8034a4b9e
AndrewJBateman <gomezbateman@yahoo.com>
2024-01-17 11:35:06 +0100

    refactor: standalone nav and dropdown use tooltip & router active

--- src/app/modules/common/nav/dropdowns/dropdown/dropdown.component.ts
+++ src/app/modules/common/nav/dropdowns/dropdown/dropdown.component.ts
@@ -1,5 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+/**
+ * Component for displaying a dropdown menu in an Angular application.
+ *
+ * This component imports the necessary modules and components from the Angular Material library
+ * to create a dropdown menu with buttons and icons. It also injects the Angular Router to handle
+ * navigation within the application.
+ *
+ * @selector app-dropdown
+ * @templateUrl ./dropdown.component.html
+ * @standalone true
+ * @imports MatButtonModule, MatMenuModule, MatIconModule, RouterLink
+ */
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
@@ -8,12 +20,11 @@ import { MatButtonModule } from '@angular/material/button';
 	selector: 'app-dropdown',
 	templateUrl: './dropdown.component.html',
 	standalone: true,
-	imports: [
-		MatButtonModule,
-		MatMenuModule,
-		MatIconModule,
-		RouterLink,
-		RouterLinkActive,
-	],
+	imports: [MatButtonModule, MatMenuModule, MatIconModule, RouterLink],
 })
-export class DropdownComponent {}
+export class DropdownComponent {
+	public router = inject(Router);
+
+	public aboutRoute = '/about';
+	public contactRoute = '/contact';
+}
--- src/app/modules/common/nav/nav.component.ts
+++ src/app/modules/common/nav/nav.component.ts
@@ -6,11 +6,21 @@
  */
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment';
+import { DropdownComponent } from './dropdowns/dropdown/dropdown.component';
+import { RatpDropdownComponent } from './dropdowns/ratp-dropdown/ratp-dropdown.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
 	selector: 'app-nav',
 	templateUrl: './nav.component.html',
 	encapsulation: ViewEncapsulation.None,
+	standalone: true,
+	imports: [
+		RouterLink,
+		RouterLinkActive,
+		RatpDropdownComponent,
+		DropdownComponent,
+	],
 })
 export class NavComponent implements OnInit {
 	name: string;
